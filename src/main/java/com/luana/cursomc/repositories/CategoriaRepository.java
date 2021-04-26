e6154dc1c1bc2b0874f1990caea3157798cff5ec
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-26 16:32:30 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/luana/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.luana.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luana.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+
+}
---
+++ src/main/java/com/luana/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.luana.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luana.cursomc.domain.Categoria;
+import com.luana.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+}
