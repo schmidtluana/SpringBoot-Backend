996745174416f8b40ad498a5d2af9eaa3566abf2
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-26 22:32:43 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/luana/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.luana.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luana.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+
+}
