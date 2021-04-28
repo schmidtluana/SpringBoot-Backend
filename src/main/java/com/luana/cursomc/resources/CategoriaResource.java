e73c165736671e3d21d706fd9e93ddee38a8cbc8
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-28 14:43:17 -0300

    Inserindo nova Categoria

--- src/main/java/com/luana/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luana/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,19 @@
 package com.luana.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luana.cursomc.domain.Categoria;
 import com.luana.cursomc.services.CategoriaService;
+
 
 @RestController
 @RequestMapping(value="/categorias") //endpoint
@@ -27,6 +29,12 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+		.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 }
