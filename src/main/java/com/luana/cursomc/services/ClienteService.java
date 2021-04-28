ed7c89fe1568840f0f37171130c808bc309f0cbb
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-27 22:04:55 -0300

    Endpoint clientes

---
+++ src/main/java/com/luana/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.luana.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luana.cursomc.domain.Cliente;
+import com.luana.cursomc.repositories.ClienteRepository;
+import com.luana.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+}
+	
+}
+
