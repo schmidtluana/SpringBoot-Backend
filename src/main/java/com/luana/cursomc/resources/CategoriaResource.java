e520c8cc175f09040f92a689aed5a1c54c5322df
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-28 16:28:12 -0300

    Listando todas as Categorias (DTO)

--- src/main/java/com/luana/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luana/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.luana.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luana.cursomc.domain.Categoria;
+import com.luana.cursomc.dto.CategoriaDTO;
 import com.luana.cursomc.services.CategoriaService;
 
 
@@ -22,6 +25,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	//busca a categoria por id
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {	
 		Categoria obj = service.find(id);
@@ -48,5 +52,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	//buscar todas as categorias
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {	
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(
+		obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/luana/cursomc/services/CategoriaService.java
+++ src/main/java/com/luana/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.luana.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,9 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DateIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
 
