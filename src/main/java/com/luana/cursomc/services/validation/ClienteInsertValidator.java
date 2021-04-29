947d8ac54b00492330bc86b4beb010b44637ccd6
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-29 20:59:17 -0300

    Validação Email

--- src/main/java/com/luana/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/luana/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.luana.cursomc.domain.Cliente;
 import com.luana.cursomc.domain.enums.TipoCliente;
 import com.luana.cursomc.dto.ClienteNewDTO;
+import com.luana.cursomc.repositories.ClienteRepository;
 import com.luana.cursomc.resources.exception.FieldMessage;
 import com.luana.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	public ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -31,6 +39,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail((objDto.getEmail()));
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
