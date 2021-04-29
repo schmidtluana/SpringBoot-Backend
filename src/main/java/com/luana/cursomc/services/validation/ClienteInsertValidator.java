db513fcae79297307bf5ff4d7c025e9996eabcba
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-29 20:15:58 -0300

    Validação customizada de CPF ou CNPJ

---
+++ src/main/java/com/luana/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,41 @@
+package com.luana.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.luana.cursomc.domain.enums.TipoCliente;
+import com.luana.cursomc.dto.ClienteNewDTO;
+import com.luana.cursomc.resources.exception.FieldMessage;
+import com.luana.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+
+		if(objDto.getTipo().equals(TipoCliente.PESSSOAFISICA.getCod()) 
+				&& !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) 
+				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
