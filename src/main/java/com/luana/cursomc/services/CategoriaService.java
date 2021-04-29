4aa53a0ce09529543621626b59297684fd192ab0
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-28 21:01:44 -0300

    Validação com Bean Validation

--- src/main/java/com/luana/cursomc/services/CategoriaService.java
+++ src/main/java/com/luana/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luana.cursomc.domain.Categoria;
+import com.luana.cursomc.dto.CategoriaDTO;
 import com.luana.cursomc.repositories.CategoriaRepository;
 import com.luana.cursomc.services.exceptions.DateIntegrityException;
 import com.luana.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,5 +53,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
 
