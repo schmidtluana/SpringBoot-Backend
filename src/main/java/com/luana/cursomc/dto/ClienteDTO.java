eab59b303cc92ca605675979d1a5f640fe5d0bc5
Luana Schmidt <luana.caroline.schmidt@gmail.com>
2021-04-29 21:33:22 -0300

    Validação Email - PUT

--- src/main/java/com/luana/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/luana/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.luana.cursomc.domain.Cliente;
+import com.luana.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
---
+++ src/main/java/com/luana/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.luana.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.luana.cursomc.domain.Cliente;
+import com.luana.cursomc.dto.ClienteDTO;
+import com.luana.cursomc.repositories.ClienteRepository;
+import com.luana.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	public ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer urlId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();	
+
+		
+		Cliente aux = repo.findByEmail((objDto.getEmail()));
+		if(aux != null && !aux.getId().equals(urlId)) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
